5b7eaeab504debcd64cfed8559335440fdc404d6
brazer <salanevich.anatol@gmail.com>
2015-04-25 15:25:22 +0300

    changed getting way of shared preferences

--- app/src/main/java/com/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sunshine/ForecastFragment.java
@@ -1,11 +1,11 @@
 package com.example.sunshine;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -95,7 +95,7 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                SharedPreferences pref = getActivity().getSharedPreferences("com.example.sunshine_preferences", Activity.MODE_PRIVATE);
+                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
                 String location = pref.getString(getResources().getString(R.string.settings_location_key), "");
                 mTask.execute(location);
                 break;
