345ad8b62d42b3446e88e7191db43082444c6e85
brazer <salanevich.anatol@gmail.com>
2015-07-04 01:44:38 +0300

    changed date format in list

--- app/src/main/java/com/example/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/sunshine/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -15,29 +16,6 @@ import android.widget.TextView;
 public class ForecastAdapter extends CursorAdapter {
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
-    }
-
-    /**
-     * Prepare the weather high/lows for presentation.
-     */
-    private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
-        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
-        return highLowStr;
-    }
-
-    /*
-        This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
-        string.
-     */
-    private String convertCursorRowToUXFormat(Cursor cursor) {
-        String highAndLow = formatHighLows(
-                cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP),
-                cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP));
-
-        return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE)) +
-                " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC) +
-                " - " + highAndLow;
     }
 
     /*
@@ -55,6 +33,8 @@ public class ForecastAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
+        ImageView icon = (ImageView) view.findViewById(R.id.list_item_icon);
+        icon.setImageResource(R.mipmap.ic_launcher);
         TextView date = (TextView) view.findViewById(R.id.list_item_date_textview);
         date.setText(getDate(cursor));
         TextView forecast = (TextView) view.findViewById(R.id.list_item_forecast_textview);
@@ -66,7 +46,8 @@ public class ForecastAdapter extends CursorAdapter {
     }
 
     private String getDate(Cursor cursor) {
-        return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
+        return Utility.getFriendlyDayString(
+                mContext, cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
     }
 
     private String getForecast(Cursor cursor) {
