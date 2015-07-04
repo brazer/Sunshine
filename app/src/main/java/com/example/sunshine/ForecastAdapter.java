9ec18efe0a9b29e77871f32f79763724d695f6c7
brazer <salanevich.anatol@gmail.com>
2015-07-04 14:45:41 +0300

    added today item list

--- app/src/main/java/com/example/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/sunshine/ForecastAdapter.java
@@ -14,6 +14,10 @@ import android.widget.TextView;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -23,7 +27,21 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if (viewType==VIEW_TYPE_TODAY) layoutId = R.layout.list_item_forecast_today;
+        else if (viewType==VIEW_TYPE_FUTURE_DAY) layoutId = R.layout.list_item_forecast;
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position==0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
     }
 
     /*
