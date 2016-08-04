591f12d452db22e3fb019bb6c8b16ca1ff3fabdb
brazer <salanevich.anatol@gmail.com>
2016-08-04 12:56:38 +0300

    added settings of notifications

--- app/src/main/java/com/example/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/sunshine/sync/SunshineSyncAdapter.java
@@ -435,6 +435,11 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
         Context context = getContext();
         //checking the last update and notify if it' the first of the day
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+        boolean displayNotifications = prefs.getBoolean(displayNotificationsKey,
+                Boolean.parseBoolean(context.getString(R.string.pref_enable_notifications_default)));
+        if (!displayNotifications) return;
+
         String lastNotificationKey = context.getString(R.string.pref_last_notification);
         long lastSync = prefs.getLong(lastNotificationKey, 0);
 
